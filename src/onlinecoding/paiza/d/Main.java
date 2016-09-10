cbff98a6e1cd30ee5d66e51bb9dbe14b355ef4c0
渡邉祥吾 <WatanabeShogo@shogo.local>
2016-09-10 09:38:26 +0900

    D049 added.

---
+++ src/onlinecoding/paiza/d/Main.java
@@ -0,0 +1,14 @@
+package onlinecoding.paiza.d;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[] ) throws Exception {
+    	Scanner sc = new Scanner(System.in);
+    	sc.nextInt();
+    	
+    	
+    	
+
+    }
+}
