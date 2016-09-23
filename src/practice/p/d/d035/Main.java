c335d6687325b91f244fba657e8d2b17caaa233c
渡邉祥吾 <WatanabeShogo@shogo.local>
2016-09-23 18:39:36 +0900

    d035 added.

---
+++ src/practice/p/d/d035/Main.java
@@ -0,0 +1,11 @@
+package practice.p.d.d035;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[] ) throws Exception {
+    	Scanner sc = new Scanner(System.in);
+    	String date = sc.nextLine().replace(" ","/");
+    	System.out.println(date);
+    }
+}
